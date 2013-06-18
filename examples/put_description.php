e4e5e71badd68ac09de081bd4d8ee7333bd09c7d
Wesley Cintra do Nascimento <cintra.wesley@gmail.com>
2013-06-18 11:02:23 -0300

    fixing examples put and delete

--- examples/put_description.php
+++ examples/put_description.php
@@ -24,7 +24,11 @@ if($_GET['code']) {
 		}
 	}
 	
-	$meli->delete('/questions/12345678', array('access_token' => $_SESSION['access_token']));
+	$params = array('access_token' => $_SESSION['access_token']);
+
+	$body = array('text' => 'Adding new description <strong>html</strong>');
+
+	$response = $meli->put('/items/MLB12343412/descriptions', $body, $params);
 	
 } else {
 	echo '<a href="' . $meli->getAuthUrl('http://localhost/PHPSDK/examples/login.php') . '">Login using MercadoLibre oAuth 2.0</a>';
